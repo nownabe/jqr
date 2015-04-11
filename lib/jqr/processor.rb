75c4192ff8cf550d666af02d42a983a1d8b4f6b9
nownabe <nownabe@gmail.com>
2015-04-12 02:13:27 +0900

    Refactor

--- lib/jqr/processor.rb
+++ lib/jqr/processor.rb
@@ -7,39 +7,55 @@ module Jqr
     end
 
     def run
-      json = queries.inject(input_json) { |json, query| eval("json#{query}") }
-      json =
-        case json
-        when Hash
-          pretty = JSON.pretty_generate(json)
-          if STDOUT.tty?
-            pretty
-              .gsub(/(?<key>".+"):/) { "#{CE.fg(:h_blue).get(Regexp.last_match[:key])}:" }
-              .gsub(/: (?<val>".+")/) { ": #{CE.fg(:green).get(Regexp.last_match[:val])}" }
-          else
-            pretty
-          end
-        when String, Numeric, TrueClass, FalseClass
-          json
-        else
-          PP.pp(json, "")
-        end
-
-      puts json
+      puts output
     end
 
     private
 
+    def colorful?
+      !options[:"no-color"]
+    end
+
+    def colorize(txt)
+      txt
+        .gsub(/(?<key>".+"):/) { "#{CE.fg(:h_blue).get(Regexp.last_match[:key])}:" }
+        .gsub(/: (?<val>".+")/) { ": #{CE.fg(:green).get(Regexp.last_match[:val])}" }
+    end
+
+    def input
+      STDIN.read
+    end
+
     def input_json
       JSON.parse(input).with_indifferent_access
+    end
+
+    def output
+      case processed_data
+      when Hash
+        tty? && colorful? ? colorize(pretty_json) : pretty_json
+      when String, Numeric, TrueClass, FalseClass
+        processed_data
+      else
+        PP.pp(processed_data, "")
+      end
+    end
+
+    def pretty_json
+      JSON.pretty_generate(processed_data)
+    end
+
+    def processed_data
+      @oprocessed_data ||=
+        queries.inject(input_json) { |json, query| eval("json#{query}") }
     end
 
     def queries
       @queries ||= options.arguments
     end
 
-    def input
-      STDIN.read
+    def tty?
+      STDOUT.tty?
     end
   end
 end
